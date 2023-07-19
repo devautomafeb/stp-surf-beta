2f70d889d7c80db24379c102dc4ebd41901d11e4
Automafeb <102158917+devautomafeb@users.noreply.github.com>
2023-07-18 21:09:55 -0300

    Update index.tsx

--- src/components/WhatsButton/index.tsx
+++ src/components/WhatsButton/index.tsx
@@ -6,7 +6,7 @@ import { FontH1, WhatsappContainer } from "./style";
 function WhatsButton() {
     return (
         <div>
-            <a href='https://wa.me/message/WVFWSTB6MIIRO1'>
+            <a href='https://wa.me/5521980984777'>
                 <WhatsappContainer>
 
                     <FontH1><FaWhatsapp /></FontH1>
